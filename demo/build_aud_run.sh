a93325b5107e4e48d8e31c144494570cee8ed105
Tsuyoshi Horo <horo@chromium.org>
2023-03-01 02:15:05 +0000

    Update BROTLI_REV and EMSDK_REV

--- demo/build_aud_run.sh
+++ demo/build_aud_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 
-readonly BROTLI_REV="9801a2c5d6c67c467ffad676ac301379bb877fc3"
-readonly EMSDK_REV="b6574f3a89dbe25d007e3fdc7681faac5a5c0403"
-readonly EMSDK_VER="3.1.21"
+readonly BROTLI_REV="ed1995b6bda19244070ab5d331111f16f67c8054"
+readonly EMSDK_REV="17f6a2ef92f198f3c9ff30d07664e4090a0ecaf7"
+readonly EMSDK_VER="3.1.32"
 
 rm -rf third_party encode.wasm encode.js
 
