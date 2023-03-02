76e8a0ceb1ea90f06e1e4a6867a121d6b258ffa0
Tsuyoshi Horo <horo@chromium.org>
2023-03-02 14:25:12 +0900

    Google search demo

--- demo/build_aud_run.sh
+++ demo/build_aud_run.sh
@@ -8,14 +8,19 @@ rm -rf third_party encode.wasm encode.js
 
 mkdir third_party
 cd third_party
+
+# Download brotli
+
 git clone https://github.com/google/brotli.git
 cd brotli
 git checkout $BROTLI_REV
 
+# Build dictionary_generator
 cd research/
 bazel build dictionary_generator
 cd ../../
 
+# Wikipedia demo
 mkdir -p wikipedia/pages_for_dict/
 mkdir -p wikipedia/pages/
 
@@ -35,6 +40,35 @@ done
 
 ./brotli/research/bazel-bin/dictionary_generator -t128k ./wikipedia/wikipedia.dict ./wikipedia/pages_for_dict/*
 
+# Google Search demo
+mkdir -p google_search/pages_for_dict/
+mkdir -p google_search/pages/
+
+UA="Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.91 Mobile Safari/537.36"
+SEARCH_URL="https://www.google.com/search?hl=en&q="
+
+i=0
+while read line; do
+  PN=$(printf "%03d" $(expr $i + 1))
+  i=$(expr $i + 1)
+  echo $line
+  echo "${SEARCH_URL}${line}"
+  curl  -H "User-Agent: $UA" -L "${SEARCH_URL}${line}" -o "./google_search/pages/$PN.html"
+done < ../search_list.txt
+
+
+i=0
+while read line; do
+  PN=$(printf "%03d" $(expr $i + 1))
+  i=$(expr $i + 1)
+  echo $line
+  echo "${SEARCH_URL}${line}"
+  curl  -H "User-Agent: $UA" -L "${SEARCH_URL}${line}" -o "./google_search/pages_for_dict/$PN.html"
+done < ../search_list_for_dict.txt
+
+./brotli/research/bazel-bin/dictionary_generator -t128k ./google_search/google_search.dict ./google_search/pages_for_dict/*
+
+# Download Emscripten SDK
 git clone https://github.com/emscripten-core/emsdk.git
 cd emsdk
 git checkout $EMSDK_REV
--- demo/server.js
+++ demo/server.js
@@ -51,7 +51,7 @@ console.log('__filename: ' + __filename);
 const THREE_JS_VERSIONS = ['r80', 'r81'];
 // const THREE_JS_VERSIONS = ['r80', 'r81', 'r82', 'r83', 'r84'];
 
-async function initilizeLib(lib, wikipediadata) {
+async function initilizeLib(lib, wikipediadata, googlesearchdata) {
   // https://developers.google.com/speed/libraries
   async function getThree(version) {
     const res = await getHttps(
@@ -63,15 +63,17 @@ async function initilizeLib(lib, wikipediadata) {
   const compress = require('./compress_workerpool');
 
   const wikipedia_dict = await fs.readFile('third_party/wikipedia/wikipedia.dict');
-  const dict_data = {
+  wikipediadata.dict = {
     data: wikipedia_dict,
     sha256: crypto.createHash('sha256').update(wikipedia_dict).digest('base64'),
     compressed: await compress.compress(wikipedia_dict),
   };
   console.log(
-    `dict_data: raw: ${dict_data.data.length} sha256: ${dict_data.sha256} compressed: ${dict_data.compressed.length} `
+    'wikipediadata.dict: ' +
+        `raw: ${wikipediadata.dict.data.length} ` +
+        `sha256: ${wikipediadata.dict.sha256} ` +
+        `compressed: ${wikipediadata.dict.compressed.length}`
   );
-  wikipediadata.dict = dict_data;
   let promises = [];
   for (let i = 1; i <= 10; ++i) {
     promises.push(
@@ -84,14 +86,45 @@ async function initilizeLib(lib, wikipediadata) {
           sbr: await compress.compressWithDict(html, wikipedia_dict),
         };
         console.log(
-          `${filename}: raw: ${filedata.raw.length} br: ${filedata.br.length} sbr: ${filedata.sbr.length} `
+          `wikipedia ${filename}: ` +
+              `raw: ${filedata.raw.length} ` +
+              `br: ${filedata.br.length} ` +
+              `sbr: ${filedata.sbr.length}`
         );
-
         wikipediadata[filename] = filedata;
         resolve();
       })
     );
   }
+
+  const google_search_dict = await fs.readFile('third_party/google_search/google_search.dict');
+  googlesearchdata.dict = {
+    data: google_search_dict,
+    sha256: crypto.createHash('sha256').update(google_search_dict).digest('base64'),
+    compressed: await compress.compress(google_search_dict),
+  };
+  for (let i = 1; i <= 10; ++i) {
+    promises.push(
+      new Promise(async (resolve) => {
+        const filename = ('000' + i).slice(-3) + '.html';
+        const html = await fs.readFile('third_party/google_search/pages/' + filename);
+        const filedata = {
+          raw: html,
+          br: await compress.compress(html),
+          sbr: await compress.compressWithDict(html, google_search_dict),
+        };
+        console.log(
+          `google search ${filename}: ` +
+              `raw: ${filedata.raw.length} ` +
+              `br: ${filedata.br.length} ` +
+              `sbr: ${filedata.sbr.length}`
+        );
+        googlesearchdata[filename] = filedata;
+        resolve();
+      })
+    );
+  }
+
   await Promise.all(promises);
 
   promises = [];
@@ -145,7 +178,8 @@ async function initilizeLib(lib, wikipediadata) {
 
 let three_js_lib = {};
 let wikipedia_data = {};
-const libinitilized = initilizeLib(three_js_lib, wikipedia_data);
+let google_search_data = {};
+const libinitilized = initilizeLib(three_js_lib, wikipedia_data, google_search_data);
 
 fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, 'public'),
@@ -250,7 +284,6 @@ fastify.get(`/wikipedia/`, async function (request, reply) {
     reply.send('not found');
     return;
   }
-  console.log("sec-available-dictionary" + request.headers["sec-available-dictionary"]);
   if (
     request.headers["sec-available-dictionary"] ==
     'sha256/' + wikipedia_data.dict.sha256
@@ -299,3 +332,87 @@ async function proxyWikipedia(request, reply) {
 
 fastify.get(`/w/*`, proxyWikipedia);
 fastify.get(`/static/*`, proxyWikipedia);
+
+// Google search demo
+
+fastify.get(`/google_search.dict`, async function (request, reply) {
+  await libinitilized;
+  reply.header('Cache-Control', 'public');
+  reply.header('content-type', 'binary/octet-stream');
+  reply.header('Can-Be-Used-As-Dictionary', '/google_search/');
+  reply.header('content-length', google_search_data.dict.compressed.length);
+  reply.header('content-encoding', 'br');
+  reply.send(Buffer.from(google_search_data.dict.compressed));
+});
+
+fastify.get(`/google_search.dict.br`, async function (request, reply) {
+  await libinitilized;
+  reply.header('Cache-Control', 'public');
+  reply.header('content-type', 'binary/octet-stream');
+  reply.header('content-length', google_search_data.dict.compressed.length);
+  //  reply.header('content-encoding', 'br');
+  reply.send(Buffer.from(google_search_data.dict.compressed));
+});
+
+fastify.get(`/google_search/`, async function (request, reply) {
+  await libinitilized;
+  reply.header('content-type', 'text/html; charset=utf-8');
+  const id = request.query['id'];
+  if (id == undefined) {
+    reply.send('no id');
+    return;
+  }
+  const data = google_search_data[request.query['id']];
+  if (data == undefined) {
+    reply.send('not found');
+    return;
+  }
+  if (
+    request.headers["sec-available-dictionary"] ==
+    'sha256/' + google_search_data.dict.sha256
+  ) {
+    reply.header('content-length', data.br.length);
+    reply.header('content-encoding', 'sbr');
+    reply.header('vary', 'sec-available-dictionary');
+    reply.send(Buffer.from(data.sbr));
+  } else {
+    reply.header('content-length', data.br.length);
+    reply.header('content-encoding', 'br');
+    reply.header(
+      'shared-dictionary-url',
+      '/google_search.dict'
+    );
+    reply.send(Buffer.from(data.br));
+  }
+});
+
+const googleCache = new LRU({
+  max: 500,
+  maxSize: 100 * 1000 * 1000,
+  sizeCalculation: (value, key) => {
+    return value.body.length;
+  },
+  ttl: 1000 * 60 * 5,
+});
+
+async function proxyGoogle(request, reply) {
+  const target_url = request.url;
+  let result = googleCache.get(target_url);
+  if (result == null) {
+    result = await getHttpsWithRes(`https://www.google.com${target_url}`);
+    googleCache.set(target_url, result);
+  } else {
+  }
+  const content_type = result.res.headers['content-type'];
+  reply.header('content-type', content_type);
+
+  const content_encoding = result.res.headers['content-encoding'];
+  if (content_encoding != undefined) {
+    reply.header('content-encoding', content_encoding);
+  }
+  reply.send(Buffer.from(result.body));
+}
+
+fastify.get(`/images/*`, proxyGoogle);
+fastify.get(`/gen_204`, proxyGoogle);
+fastify.get(`/xjs/*`, proxyGoogle);
